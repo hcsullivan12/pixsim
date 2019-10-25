e831f99e0e0d834500b1f88ac72ec9e6d6e3fe02
Hunter Sullivan <hcsullivan12@yahoo.com>
2019-10-25 17:02:25 -0500

    Update

--- python/pixsim/driftsim.py
+++ python/pixsim/driftsim.py
@@ -9,9 +9,10 @@ _pixel_spacing = 0.4
 _eps = np.round(np.sqrt(2*(0.5*_pixel_spacing)**2), 2)
 _step_zs, _step_ys = Set(), dict()
 
+_hDiffusion = ROOT.TH2D('_hDiffusion', '_hDiffusion', 50, 0, 50, 400, 0, 400)
+
 def is_accurate(dist, eps=_eps):
     """Check against largest distance from pixel center"""
-    #print _eps
     return dist < eps
 
 def distance2d(pos1, pos2):
@@ -129,35 +130,93 @@ def check_bipolar(pos0, pixels_y, pixels_z, pix_pos, rsp_data):
 
     return is_bip, resp, ep, dist
 
-def plot(wvfs, threshold=0.7, sch_time=0.02):
+def integrate_waveform(ts, ys, threshold=0.7, sch_time=0.02):
+    """Integrate waveform and handle resets"""
+
+    ys_int, ys_sch, sch_hits = list(ys), list(ys), list()
+    charge = 0 
+
+    for tbin in range(0, len(ys_int)):
+        charge += ys[tbin] * sch_time
+        if charge > threshold:
+            charge = 0
+            ys_sch[tbin] = 1.0
+            sch_hits.append(ts[tbin])
+        ys_int[tbin] = charge
+    
+    return ys_int, ys_sch, sch_hits
+
+def reconstruct(sch_hits, sch_time=0.02):
+    """Reconstruct waveform from resets"""
+
+    reco_data = {t:0 for t in ts}
+    for tbin in range(1, len(sch_hits)):
+        pre_time = sch_hits[tbin-1] 
+        cur_time = sch_hits[tbin] 
+        delta_t = cur_time-pre_time
+        for tick in np.arange(pre_time, cur_time, int(100*sch_time)):
+            reco_data[tick] = 100. * threshold / delta_t
+    
+    return reco_data
+
+def analyze(wvfs):
+    """Area for analysis"""
+    import matplotlib.pyplot as plt
+    global _hDiffusion
+    
+    for pid, wvf in wvfs.iteritems():
+        ts = wvf.keys()
+        ts.sort()
+        ys = [wvf[t] for t in ts]
+
+        ys_int, ys_sch, sch_hits = integrate_waveform(ts, ys)
+        if len(sch_hits) < 2:
+            continue
+        
+        #reco_data = reconstruct(sch_hits) 
+        #reco_ys = [reco_data[t] for t in ts]
+
+        sch_hits.sort()
+        time_diff = (sch_hits[-1] - sch_hits[0]) / 100
+        xpos = (sch_hits[0]/100 + 0.5 * time_diff) * 0.1648
+
+        _hDiffusion.Fill(time_diff, xpos)
+
+        #_xs.append(time_diff)
+        #_ys.append(xpos)
+    
+    #def best_fit(X, Y):
+        #xbar = sum(X)/len(X)
+        #ybar = sum(Y)/len(Y)
+        #n = len(X) # or len(Y)
+        #
+        #numer = sum([xi*yi for xi,yi in zip(X, Y)]) - n * xbar * ybar
+        #denum = sum([xi**2 for xi in X]) - n * xbar**2
+        #
+        #b = numer / denum
+        #a = ybar - b * xbar
+        #
+        #print('best fit line:\ny = {:.2f} + {:.2f}x'.format(a, b))
+        #
+        #return a, b
+    #
+    #a, b = best_fit(_xs, _ys)
+    #
+    #yfit = [a + b * xi for xi in _xs]
+    #plt.scatter(_xs, _ys)
+    #plt.plot(_xs, yfit)
+    #plt.show()
+
+def plot(wvfs):
     import matplotlib.pyplot as plt
     for pid, wvf in wvfs.iteritems():
         ts = wvf.keys()
         ts.sort()
         ys = [wvf[t] for t in ts]
 
-        #integrate
-        ys_int, ys_sch = list(ys), [0]*len(ys)
-        sch_hits = list()
-        charge = 0 
-        for tbin in range(0, len(ys_int)):
-            charge += ys[tbin] * sch_time
-            if charge > threshold:
-                charge = 0
-                ys_sch[tbin] = 1.0
-                sch_hits.append(ts[tbin])
-            ys_int[tbin] = charge
-
-        # reconstructing
-        reco_data = {t:0 for t in ts}
-        for tbin in range(1, len(sch_hits)):
-            pre_time = sch_hits[tbin-1] 
-            cur_time = sch_hits[tbin] 
-            delta_t = cur_time-pre_time
-            #print pre_time, cur_time, delta_t
-            for tick in np.arange(pre_time, cur_time, int(100*sch_time)):
-                reco_data[tick] = 100. * threshold / delta_t 
-
+        ys_int, ys_sch, sch_hits = integrate_waveform(ts, ys) 
+        
+        reco_data = reconstruct(sch_hits) 
         reco_ys = [reco_data[t] for t in ts]
 
         fig, (ax1, ax2) = plt.subplots(2, sharex=True, figsize=(15, 15))
@@ -166,6 +225,9 @@ def plot(wvfs, threshold=0.7, sch_time=0.02):
         ax2.step(ts, reco_ys)
         ax1.step(ts, ys_int)
         ax1.step(ts, ys_sch)
+        plt.xlabel('Time [us]',fontsize=15)
+        ax2.set_ylabel('Current [nA]',fontsize=15)
+        ax1.set_ylabel('Voltage [V]',fontsize=15)
         #ax2.set_ylim([0,3.5])
         plt.show()
 
@@ -209,7 +271,6 @@ def fill_map(entry, pixels_y, pixels_z, rsp_data):
             continue
 
         # we have pid, unipolar rsp, chg tick
-        print resp
         to_store = {'el':numEl, 'rsp':resp['rsp'], 'area':resp['area'], 't':time_tick, 'max':resp['max']} 
         if near_pix_id not in pid_to_wvf.keys():
             pid_to_wvf[near_pix_id] = [to_store]
@@ -217,62 +278,63 @@ def fill_map(entry, pixels_y, pixels_z, rsp_data):
             pid_to_wvf[near_pix_id].append(to_store)
     return pid_to_wvf
 
-def calculate(entry, pixels_y, pixels_z, rsp_data, rsp_time_bin=0.02):
-    """
-    Calculate waveforms for each charge cloud. 
-    Convolves charge deposits with field/electronics
-    response function.
-    """
+def convolve(pid_to_wvf, rsp_time_bin=0.02):
+    """Perform convolution of charge deposits and response function"""
 
-    pid_to_wvf = fill_map(entry, pixels_y, pixels_z, rsp_data)
+    to_fC_convers = 1.6 / 10000
 
-    # convolve
-    pid_count = 0
+    print 'Convolving...'
     for pid, dlist in pid_to_wvf.iteritems():
-        pid_count += 1
-        print 'Convolve', pid_count, '/', len(pid_to_wvf)
-
-        if len(dlist) < 25:
-            continue
-
         dlist.sort(key=lambda x: x['t'])
+
         for store in dlist:
-            nel = store['el']
-            area = rsp_time_bin * store['area']
-            store['rsp'] *= nel * 1.6 / (10000 * area * rsp_time_bin)
+            rsp_area = store['area'] * rsp_time_bin
+            chg_in_fC = store['el'] * to_fC_convers
+            store['rsp_convolved'] = store['rsp'] * chg_in_fC / rsp_area
+
+    return pid_to_wvf
+
+def make_waveforms(pid_to_wvf, rsp_time_bin=0.02):
+    """Construct waveforms by taking superposition of responses"""
 
     base = int(100 * rsp_time_bin)
     def round_it(x, base=base):
         return int(base * np.floor(float(x)/base))
 
-    # make waveform
-    pid_count = 0
     wvfs = dict()
+
+    print 'Making waveforms...'
     for pid, dlist in pid_to_wvf.iteritems():
-        pid_count += 1
         wvf = dict()
-        print 'Waveform', pid_count, '/', len(pid_to_wvf)
-
-        if len(dlist) < 25:
-            continue
-
+        
         for store in dlist:
             # start time
             tick = 100 * (store['t'] - rsp_time_bin * (store['max'] + 1))
             tick = round_it(tick)
-            print store
-
-            for tbin in range(0, len(store['rsp'])):
+            
+            for tbin in range(0, len(store['rsp_convolved'])):
                 if tick not in wvf:
-                    wvf[tick] = store['rsp'][tbin]
+                    wvf[tick] = store['rsp_convolved'][tbin]
                 else:
-                    wvf[tick] += store['rsp'][tbin]
+                    wvf[tick] += store['rsp_convolved'][tbin]
                 tick += 100 * rsp_time_bin 
                 tick = round_it(tick)
-                #print store['rsp'][tbin]
+
         wvfs[pid] = wvf
-        
-    plot(wvfs)
+
+    return wvfs
+
+def calculate(entry, pixels_y, pixels_z, rsp_data):
+    """
+    Calculate waveforms for each charge cloud. 
+    Convolves charge deposits with field/electronics
+    response function.
+    """
+    pid_to_wvf = fill_map(entry, pixels_y, pixels_z, rsp_data)
+    pid_to_wvf = convolve(pid_to_wvf)
+    wvfs = make_waveforms(pid_to_wvf)
+    #plot(wvfs)
+    analyze(wvfs)
 
 def get_pixels(entry):
     """Get pixel coordinates from ntuple"""
@@ -303,7 +365,6 @@ def prepare_data(response):
         sy, sz = sp[1], sp[2]
         sat_data = {'ep':ep, 'rsp':fn, 'area':np.sum(fn), 'max':np.argmax(fn)}
 
-
         if sz not in rsp_data.keys():
             rsp_data[sz] = {sy:sat_data}
         elif sy not in rsp_data[sz].keys():
@@ -313,7 +374,8 @@ def prepare_data(response):
     return rsp_data
 
 def sim(response, ntuple=None, **kwds):
-    """This method will loop over the events in an ntuple.
+    """
+    This method will loop over the events in an ntuple.
     The ntuple, created with larg4, is expected to have
     the following information: 
         1) 3D coordinates of charge clouds close to 
@@ -336,4 +398,7 @@ def sim(response, ntuple=None, **kwds):
     for entry in rtree:
         print 'drifting event', entry.event
         calculate(entry, pixels_y, pixels_z, rsp_data)
-        break
+
+    _canvas = ROOT.TCanvas('_canvas', '_canvas', 800, 800)
+    _hDiffusion.Draw('colz')
+    wait = input()
